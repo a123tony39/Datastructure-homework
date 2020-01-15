81a70c2b788c0e13094f5506072c37e91615653d
a123tony39 <53037529+a123tony39@users.noreply.github.com>
2020-01-15 23:33:19 +0800

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
 int main(){
+    //宣告變數
     int n;
     double n1;
+    //讀取資料直到檔案結尾
     while(scanf("%d",&n)!=EOF){
+        //將英里轉公里
         n1=(double)n*1.6;
+        //印出
         printf("%.1lf\n",n1);
     }
     return 0;
